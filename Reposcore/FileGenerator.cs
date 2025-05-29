00a49e409576404eef242eddb9301cbebe56e611
고승재 <167101247+KohSeungJae@users.noreply.github.com>
2025-05-29 09:23:41 +0000

    텍스트 파일 생성기능 구현

--- Reposcore/FileGenerator.cs
+++ Reposcore/FileGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using ConsoleTables;
 
 public class FileGenerator
 {
@@ -19,7 +20,7 @@ public class FileGenerator
     }
 
     public void GenerateCsv()
-    {      
+    {
         // 경로 설정
         string filePath = Path.Combine(_folderPath, $"{_repoName}.csv");
         using StreamWriter writer = new StreamWriter(filePath);
@@ -34,6 +35,37 @@ public class FileGenerator
             writer.WriteLine(line);
         }
 
+        Console.WriteLine($"{filePath} 생성됨");
+    }
+    public void GenerateTable()
+    {
+        // 출력할 파일 경로
+        string filePath = Path.Combine(_folderPath, $"{_repoName}.txt");
+
+        // 테이블 생성
+        var headers = "UserId,f/b_PR,doc_PR,typo,f/b_issue,doc_issue,total".Split(',');
+
+        // 각 칸의 너비 계산 (오른쪽 정렬을 위해 사용)
+        int[] colWidths = headers.Select(h => h.Length).ToArray();
+
+        var table = new ConsoleTable(headers);
+
+        // 내용 작성
+        foreach (var (id, scores) in _scores)
+        {
+            table.AddRow(
+                id.PadRight(colWidths[0]), // 글자는 왼쪽 정렬                   
+                scores.PR_fb.ToString().PadLeft(colWidths[1]), // 숫자는 오른쪽 정렬
+                scores.PR_doc.ToString().PadLeft(colWidths[2]),
+                scores.PR_typo.ToString().PadLeft(colWidths[3]),
+                scores.IS_fb.ToString().PadLeft(colWidths[4]),
+                scores.IS_doc.ToString().PadLeft(colWidths[5]),
+                scores.total.ToString().PadLeft(colWidths[6])
+            );
+        }
+
+        // 파일 출력
+        File.WriteAllText(filePath, table.ToMinimalString());
         Console.WriteLine($"{filePath} 생성됨");
     }
 }
