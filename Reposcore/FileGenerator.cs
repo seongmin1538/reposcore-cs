0e55507609c4bdd79a6646cbc550b160f83e1a99
jaewon <jaewono786@gmail.com>
2025-06-06 19:02:43 +0900

    출력 파일을 한 폴더로 통합

--- Reposcore/FileGenerator.cs
+++ Reposcore/FileGenerator.cs
@@ -13,10 +13,8 @@ public class FileGenerator
     {
         _scores = repoScores;
         _repoName = repoName;
-        _folderPath = folderPath;
-
-        // 폴더생성
-        Directory.CreateDirectory(folderPath);
+        _folderPath = Path.Combine(folderPath, repoName);
+        Directory.CreateDirectory(_folderPath);
     }
 
     double sumOfPR
@@ -54,7 +52,7 @@ public class FileGenerator
     public void GenerateTable()
     {
         // 출력할 파일 경로
-        string filePath = Path.Combine(_folderPath, $"{_repoName}.txt");
+        string filePath = Path.Combine(_folderPath, $"{_repoName}1.txt");
 
         // 테이블 생성
         var headers = "UserId,f/b_PR,doc_PR,typo,f/b_issue,doc_issue,PR_rate,IS_rate,total".Split(',');
